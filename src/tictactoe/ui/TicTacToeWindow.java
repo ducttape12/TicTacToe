1985afa46846ab3aaf9def90d7cf43622e452e0d
Keith Ott <ducttape12@yahoo.com>
2020-08-29 12:09:31 -0500

    Removed unneeded reference

--- src/tictactoe/ui/TicTacToeWindow.java
+++ src/tictactoe/ui/TicTacToeWindow.java
@@ -5,7 +5,6 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import javax.swing.JFrame;
