c2dbc82452f65d82b67d1ea4d2d649040cf5b41d
Keith Ott <ducttape12@yahoo.com>
2020-09-09 08:09:09 -0500

    Can now start a new game

--- src/tictactoe/ui/TicTacToeWindow.java
+++ src/tictactoe/ui/TicTacToeWindow.java
@@ -1,6 +1,7 @@
 package tictactoe.ui;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,10 +9,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.SwingConstants;
+import javax.swing.JPanel;
 import javax.swing.Timer;
 import javax.swing.UIManager;
 
@@ -30,9 +32,11 @@ public class TicTacToeWindow extends JFrame implements ActionListener {
 	
 	private Board board;
 	private BoardDisplay boardDisplay;
-	private JLabel statusMessage;
 	private List<Player> players;
 	private int currentPlayerIndex;
+	private JPanel bottomLayout;
+	private JLabel statusMessage;
+	private JButton newGame;
 	
 	private Timer computerPlayerDelay;
 	private int COMPUTER_THINKING_DELAY_MILLISECONDS = 750;
@@ -41,26 +45,45 @@ public class TicTacToeWindow extends JFrame implements ActionListener {
 		super("Tic Tac Toe");
 		
 		setLookAndFeel();
-		board = new Board();
-		initializePlayers();
+		
+		setLayout(new BorderLayout());
 		
 		computerPlayerDelay = new Timer(COMPUTER_THINKING_DELAY_MILLISECONDS, this);
 		computerPlayerDelay.setRepeats(false);
 		
-		setLayout(new BorderLayout());
-		boardDisplay = new BoardDisplay(board);
-		add(boardDisplay, BorderLayout.CENTER);
-		boardDisplay.addCellClickedActionListener(this);
-		
-		statusMessage = new JLabel();
-		statusMessage.setFont(new Font("Sans Serif", Font.PLAIN, 20));
-		statusMessage.setHorizontalAlignment(SwingConstants.CENTER);
-		add(statusMessage, BorderLayout.SOUTH);
+		initializeNewGame();
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(640, 480);
 		setLocationRelativeTo(null);
 		setVisible(true);
+	}
+	
+	private void initializeNewGame() {
+		board = new Board();
+		initializePlayers();
+		
+		if(boardDisplay != null) {
+			remove(boardDisplay);
+		}
+		if(bottomLayout != null) {
+			remove(bottomLayout);
+		}
+		boardDisplay = new BoardDisplay(board);
+		add(boardDisplay, BorderLayout.CENTER);
+		boardDisplay.addCellClickedActionListener(this);
+		
+		bottomLayout = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 0));
+		
+		statusMessage = new JLabel();
+		statusMessage.setFont(new Font("Sans Serif", Font.PLAIN, 20));
+		bottomLayout.add(statusMessage);
+		
+		newGame = new JButton("New Game");
+		newGame.addActionListener(this);
+		bottomLayout.add(newGame);
+		
+		add(bottomLayout, BorderLayout.PAGE_END);
 		
 		setDescription();
 		
@@ -73,9 +96,7 @@ public class TicTacToeWindow extends JFrame implements ActionListener {
 		
 		Object[] playerOptions = { "Human", "Easy Computer", "Hard Computer" };
 		
-		int result = 0;
-		do {
-			result = JOptionPane.showOptionDialog(null,
+		int result = JOptionPane.showOptionDialog(this,
 					"Who would you like to play against?",
 					"Tic Tac Toe",
 					JOptionPane.YES_NO_CANCEL_OPTION,
@@ -84,22 +105,21 @@ public class TicTacToeWindow extends JFrame implements ActionListener {
 					playerOptions,
 					null);
 
-		} while(result != JOptionPane.YES_OPTION &&
-				result != JOptionPane.NO_OPTION &&
-				result != JOptionPane.CANCEL_OPTION);
-
 		Player player1 = new Player("Player 1", Mark.PLAYER_1);
-		Player player2;
+		Player player2 = null;
 		
-		// Human
 		if(result == JOptionPane.YES_OPTION) {
 			player2 = new Player("Player 2", Mark.PLAYER_2);
 			
-		// Easy Computer
 		} else if (result == JOptionPane.NO_OPTION) {
 			player2 = new Player("Easy Computer", Mark.PLAYER_2, new EasyComputerPlayer());
-		} else {
+			
+		} else if (result == JOptionPane.CANCEL_OPTION) {
 			player2 = new Player("Hard Computer", Mark.PLAYER_2, new HardComputerPlayer());
+			
+		} else {
+			System.exit(0);
+			
 		}
 		
 		players = new ArrayList<Player>();
@@ -128,7 +148,17 @@ public class TicTacToeWindow extends JFrame implements ActionListener {
 		if(e.getSource() == computerPlayerDelay) {
 			makeComputerPlayerMove();
 			
-		} else if(e.getActionCommand() != null && e.getActionCommand().equals("Cell") && !isCurrentPlayerComputer()) {
+		} else if (e.getSource() == newGame) {
+			int result = JOptionPane.showConfirmDialog(this,
+					"Do you want to start a new game?",
+					"Start a New Game?",
+					JOptionPane.YES_NO_OPTION);
+			
+			if(result == JOptionPane.YES_OPTION) {
+				 initializeNewGame();
+			}
+			
+		} if(e.getActionCommand() != null && e.getActionCommand().equals("Cell") && !isCurrentPlayerComputer()) {
 			CellDisplay cell = (CellDisplay)e.getSource();
 			makeMark(cell.getCoordinate());
 		}
@@ -199,6 +229,7 @@ public class TicTacToeWindow extends JFrame implements ActionListener {
 	
 	private void queueComputerPlayerMove() {
 		computerPlayerDelay.start();
+		newGame.setEnabled(false);
 	}
 	
 	private void makeComputerPlayerMove() {
@@ -209,6 +240,7 @@ public class TicTacToeWindow extends JFrame implements ActionListener {
 		Coordinate move = ai.getMove(board, mark);
 		
 		makeMark(move);
+		newGame.setEnabled(true);
 	}
 	
 	private boolean isCurrentPlayerComputer() {
